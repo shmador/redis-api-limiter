38f54f78adb6b4bfb0855bfa4bb0fe04f1c61d79
shmador <dorattar4@gmail.com>
2025-06-29 19:30:03 +0300

    Updated cache time

--- service.py
+++ service.py
@@ -3,7 +3,7 @@ import redis
 import json
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True, password='eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81')
-cache = r.get('foo')
+cache = r.get('cache')
 
 if cache:
     print(json.loads(cache))
@@ -11,6 +11,6 @@ else:
     url = "https://jsonplaceholder.typicode.com/todos/2"
     response = requests.get(url)
     data = response.json()
-    r.set('foo', json.dumps(data), ex=1)
+    r.set('cache', json.dumps(data), ex=10)
     print(data)
 
